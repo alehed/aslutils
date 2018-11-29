9430f8a7744e4ffda5ef2ed6e3bbea27c6bc503e
Alexander Hedges <ahedges@ethz.ch>
2018-11-29 16:47:08 +0100

    Update setup script to detect antlr version and install antlr runtime

--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # AslUtils
 
 Utility scripts to parse ASL files.
+
+To install and use the tool, preferably into a virtualenv:
+
+1. Install antlr4 (manually or via the package manager of your choice)
+
+1. (Optional) Setup a virtual environment from the working top level directory: `python3 -m venv env/` and activate it `source env/bin/activate` (activation has to happen every time you want to use it).
+
+1. `pip install -e /path/to/aslutils`
--- setup.py
+++ setup.py
@@ -1,5 +1,35 @@
 import setuptools
 
+import re
+import subprocess
+import sys
+
+# Find antlr version for runtime dep
+antlr_version = ""
+try:
+    antlr = subprocess.run(["antlr4"], stdout=subprocess.PIPE)
+    if antlr.returncode != 0:
+        print("Could not find antlr4 installation. Unable to proceed", file=sys.stderr)
+        exit(1)
+    print(antlr.stdout.decode("utf-8"))
+    match = re.match(r"ANTLR Parser Generator  Version 4\.(\d+)\.", antlr.stdout.decode("utf-8"))
+    assert match
+    antlr_version = "==4." + match.groups()[0] + ".*"
+except OSError as e:
+    print("Execution failed:", e, file=sys.stderr)
+    exit(1)
+
+# Run Antrl
+try:
+    antlr = subprocess.run(["antlr4", "-Dlanguage=Python3", "-no-listener", "-visitor", "./aslutils/ASL.g4"], stdout=subprocess.PIPE)
+    if antlr.returncode != 0:
+        print("Could not run antlr4. Unable to proceed", file=sys.stderr)
+        exit(1)
+except OSError as e:
+    print("Execution failed:", e, file=sys.stderr)
+    exit(1)
+
+
 with open("readme.md", "r") as fh:
     long_description = fh.read()
 
@@ -13,6 +43,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://code.systems.ethz.ch/diffusion/ASLUTIL/aslutils.git",
     packages=setuptools.find_packages(),
+    install_requires=['antlr4-python3-runtime{0}'.format(antlr_version)],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
